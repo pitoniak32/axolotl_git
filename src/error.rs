1b301107d46526a121e0d384408fb3fddcfb3349
David Pitoniak <davidpitoniak32@gmail.com>
2024-04-24 11:06:10 -0400

    feat: improve fzf selection errors

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,11 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
-pub enum AxlError {
+pub enum Error {
     #[error("could not find any sessions to choose from")]
     NoSessionsFound,
 
-    #[error("no project was selected from the list, cannot proceed")]
+    #[error("no matching project was selected from the list, cannot proceed")]
     NoProjectSelected,
 
     #[error("provided config path does not exist")]
